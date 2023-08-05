d6488893a2086c3607bf77a6aab222dde5cee3a8
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-08-05 23:46:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def find_closest_word(user_input, algorithm_data):
 
         return closest_word, max_similarity
     else:
-        return ""
+        return "lire()"
 
 
 # Function to get examples
@@ -63,7 +63,7 @@ def get_example(user_input, definition):
 
         return closest_word_exe, max_similarity_exe
     else:
-        return ""
+        return "lire()"
 
 def algortitheme():
         user_input = st.chat_input("algorithm :")
