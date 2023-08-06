22a49c2543d381bc67192355e38d92d16470d4d6
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-08-06 21:30:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -209,8 +209,9 @@ def main():
     st.write("Free Research Preview. [Algorithm.ai August 6 Version](#).")
     st.write("---")
     col1, col2 = st.columns([2, 5])
+    """<iframe src="https://trinket.io/embed/python/cb60355528?runMode=console" width="100%" height="356" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>"""
     with col2:
-    "<iframe src="https://trinket.io/embed/python/cb60355528?runMode=console" width="100%" height="356" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>"
+    
         st.write(":copyright:2023 by Algorithm.ai | DevTunisian")
 
     col4, col3 = st.sidebar.columns([3, 7])
